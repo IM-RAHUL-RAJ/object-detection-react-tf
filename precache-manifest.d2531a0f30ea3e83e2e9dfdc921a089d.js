9124dec26af0fbe81fb81b6446a33cd969a7f87a
Rahul Raj <rahuldps3313@gmail.com>
2022-09-24 03:11:18 +0530

    Updates

---
+++ precache-manifest.d2531a0f30ea3e83e2e9dfdc921a089d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33918b3844adca558e80b7d2b7f656ab",
+    "url": "/object-detection-react-tf/index.html"
+  },
+  {
+    "revision": "dbd135d15ca3b155e306",
+    "url": "/object-detection-react-tf/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "912e23021594bec7bd2d",
+    "url": "/object-detection-react-tf/static/js/2.2e2b7c43.chunk.js"
+  },
+  {
+    "revision": "b183437b48fb0c7c10973acfa520fbc1",
+    "url": "/object-detection-react-tf/static/js/2.2e2b7c43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbd135d15ca3b155e306",
+    "url": "/object-detection-react-tf/static/js/main.5d7593f2.chunk.js"
+  },
+  {
+    "revision": "89a1663f4b489a50d913",
+    "url": "/object-detection-react-tf/static/js/runtime-main.60e7a9c9.js"
+  }
+]);
